600361ab2972827a3140c120852e1ab390a069e7
jujuluxs <sufyannor2016@gmail.com>
2023-06-12 15:47:27 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package hitung;
 
-/**
- *
- * @author USER
- */
 public class MatematikaBeraksi {
+    public static void main (String[]args){
+   //membuat objek
+   Matematika sufyan = new Matematika(8,1);
+        System.out.println("Hasil penjumlahan: ="+ sufyan.setPenjumlahan());
+        System.out.println("Hasil pengurangan:  ="+ sufyan.setPengurangan());
+        System.out.println("Hasil perkalian: ="+ sufyan.setPerkalian());
+        System.out.println("Hasil pembagian: ="+ sufyan.setPembagian());
+        
+        
+    }
     
 }
