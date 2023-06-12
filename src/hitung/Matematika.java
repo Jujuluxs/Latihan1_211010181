dd9b602509b485dfef2107fd07343c45a348c3ff
jujuluxs <sufyannor2016@gmail.com>
2023-06-12 15:43:12 +0800

    Melengkapi method Matematika

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -14,6 +14,17 @@ public class Matematika {
         return bil1 + bil2;
         
     }
-
+public double setPengurangan (){
+        return bil1 - bil2;
+        
+    }
+    public double setPerkalian (){
+        return bil1 * bil2;
+        
+    }
+    public double setPembagian (){
+        return bil1 / bil2;
+        
+    }
     
 }
